956996b7e6cacd9cdbeb22f97f124ce349ea4325
Pavel <92167532+P4wtet@users.noreply.github.com>
2023-01-05 15:41:10 +0300

    Add files via upload
    Добавил проект сделаный по подобию галереи, использовал только спецификацию flexbox.

---
+++ flex-container/script.js
@@ -0,0 +1,81 @@
+/*alert("Hello my website");*/
+
+/*
+const user = {
+    name: "Pavel",
+    age: 18,
+    surname: "Hmelkov",
+}
+
+console.log(user);
+
+user.age = 21;
+
+console.log(user);
+*/
+
+/*
+let hello = "Hello World";
+console.log(hello);*/
+
+/*let user = "Вася";
+
+console.log(user);
+
+let userName = "Вася";
+*/
+
+/*
+const BLOCK_SIZE = 14 + 50;
+
+console.log(BLOCK_SIZE);
+
+const BLOCK_HEIGHT = 100;
+
+console.log(BLOCK_HEIGHT);
+*/
+
+/*
+if (true) {
+    var size = 15;
+}
+
+console.log(size);
+*/
+
+/*
+let meaning = 937;
+console.log(typeof meaning);
+*/
+
+/*
+let plus = 1 / 0;
+console.log(typeof plus);
+*/
+
+/*
+alert(1 / 0);
+*/
+
+/*
+alert(NaN * 1);
+*/
+
+/*
+const big = 12378168638679829389800207475687179178947657868927642786487657625901n;
+console.log(big);
+*/
+
+/*
+let name = 'Pavel';
+alert(`Привет ${name}`)
+*/
+
+/*
+let calculationsTrue = 15 > 5;
+alert(calculationsTrue);
+
+let calculationsFalse = 15 < 5;
+alert(calculationsFalse);
+*/
+
